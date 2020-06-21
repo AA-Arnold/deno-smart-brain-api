e06e4196691a779134cd128fae5fdfaba4e5d19a
Arnold Amasoh <amasoharnold@gmail.com>
2020-06-21 03:20:56 -1100

    adding atlas

--- database.ts
+++ database.ts
@@ -1,7 +1,10 @@
 import { MongoClient } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
 
 const client = new MongoClient();
-client.connectWithUri("mongodb://127.0.0.1:27017");
+client.connectWithUri(
+  "mongodb+srv://arnold:Rodaxboy92@arnold-0wgw5.mongodb.net/arnold?retryWrites=true&w=majority",
+);
+// mongodb://127.0.0.1:27017
 const db = client.database("smart-brains");
 
 export default db;
